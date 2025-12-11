889a9ec85039e6653bda0cce0c39005752a1de0b
MOLA <91349290+RedThek@users.noreply.github.com>
2025-12-11 22:44:41 +0100

    Update into working implementation of encryption and decryption logic in C and begining of C++ implementation

--- src/c/main.c
+++ src/c/main.c
@@ -3,56 +3,41 @@
 #include <string.h>
 #include <ctype.h>
 
-char* encrypt(char* message, int key);
-char* decrypt(char* message, int key);
+void encrypt(char* message, int key);
+void decrypt(char* message, int key);
 
 int main() {
-    char* message = "Hello World";
+    char message[] = "Hello World";
     int key = 3;
-    printf("The encrypted message is : %s", encrypt(message, key));
+    printf("The original message is : %s \n", message);
+    encrypt(message, key);
+    printf("The encrypted message is : %s \n", message);
+    decrypt(message, key);
+    printf("The decrypted message is : %s \n", message);
     return 0;
 }
 
-char* encrypt(char* message, int key)
+void encrypt(char* message, int key)
 {
-    int alphabet = 26;
-    int message_length = strlen(message);
-    char base_character, encrypted_character;
-    char* encrypted_message[message_length];
+    int alphabet = 26, message_length = strlen(message), base_character, current_character, encrypted_character;
 
     key = key % alphabet;
-    if (key < 0)
-    {
-        key = key + alphabet;
-    }
+    if (key < 0) key = key + alphabet;
     
     for (int i = 0; i < message_length; i++)
     {
-        if (isalpha(message[i]) == 1)
+        if (isalpha(message[i]))
         {
-            if(isupper(message[i]) == 1){
-                base_character = 'A';
-                encrypted_character = (message[i] - base_character + key) % alphabet + base_character;
-                encrypted_message[i] = encrypted_character;
-                encrypted_message[i + 1] = '\0';
-            } else {
-                base_character = 'a';
-                encrypted_character = (message[i] - base_character + key) % alphabet + base_character;
-                encrypted_message[i] = encrypted_character;
-                encrypted_message[i + 1] = '\0';
-            }
-        } else {
-            encrypted_message[i] = message[i];
-            encrypted_message[i + 1] = '\0';
+            if(islower(message[i])) base_character = 'a';
+            if(isupper(message[i])) base_character = 'A';
+            encrypted_character = (message[i] - base_character + key) % alphabet + base_character;
+            message[i] = (char)encrypted_character;
         }
-        
     }
-
-    return encrypted_message;
 }
 
-char* decrypt(char* message, int key)
+void decrypt(char* message, int key)
 {
     int alphabet = 26;
-    return encrypt(message, alphabet - key);
+    encrypt(message, alphabet - key);
 }
