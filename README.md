0fc864db38475109cbba324cfa7441b102a629f5
MOLA <91349290+RedThek@users.noreply.github.com>
2025-12-18 20:23:43 +0100

    Refactor PHP encryption logic for improved readability and efficiency; update algorithm descriptions in README and algorithm file

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 A description of the classic caesar cipher with pseudo code in english and french including it's implementation in various common programming languages.
 
-## PHP implementation
-
-The PHP implementation of this cipher was made using php8.4
-
 ## C implementation
 
 The C implementation of this cipher was made using the gcc compiler compatible with the c23 standard.
@@ -13,3 +9,15 @@ The C implementation of this cipher was made using the gcc compiler compatible w
 ## C++ implementation
 
 The C++ implementation of this cipher was made using the g++ compiler compatible with the c23 standard.
+
+#Java implementation
+
+The Java implementation of this cipher was made using Java SE 23 and JDK 23.
+
+#Javascript implementation
+
+The Javascript implementation of this cipher was made using Javascript ES6 and nodeJS.
+
+## PHP implementation
+
+The PHP implementation of this cipher was made using php8.4
--- src/php/index.php
+++ src/php/index.php
@@ -6,48 +6,30 @@ function encrypt(string $message, int $key): string
 {
     $alphabet = 26;
     $encrypted_message = "";
-    $message_length = strlen($message);
-
     $key = $key % $alphabet;
 
     if($key < 0){
-        $key = $key + $alphabet;
+        $key = $alphabet + $key;
     }
+    $key = $key + $alphabet;
+    $encrypted_message = preg_replace_callback('/[a-zA-Z]/', function ($matches) use ($key, $alphabet) {
+        $char = $matches[0];
+        $base = ctype_lower($char) ? ord('a') : ord('A');
+        $current_character = ord($char);
+        $encrypted_character = ($current_character - $base + $key) % $alphabet + $base;
+        return chr($encrypted_character);
+    }, $message);
 
-    for ($i = 0; $i < $message_length; $i++){
-        if (ctype_alpha($message[$i])){
-            if (ctype_lower($message[$i])){
-                $base_character = ord('a');
-                $current_character = ord($message[$i]);
-                $encrypted_character = ($current_character - $base_character + $key) % $alphabet + $base_character;
-                $encrypted_message = $encrypted_message.chr($encrypted_character);
-            } elseif (ctype_upper($message[$i])) {
-                $base_character = ord('A');
-                $current_character = ord($message[$i]);
-                $encrypted_character = ($current_character - $base_character + $key) % $alphabet + $base_character;
-                $encrypted_message = $encrypted_message.chr($encrypted_character);
-            } else {
-                $encrypted_message = $encrypted_message.$message[$i];
-            }
-        } else {
-            $encrypted_message = $encrypted_message.$message[$i];
-        }
-    }
     return $encrypted_message;
 }
 
 function decrypt(string $message, int $key): string
 {
-    $alphabet = 26;
-    return encrypt($message, $alphabet - $key);
+    return encrypt($message, 26 - $key);
 }
 
-echo "Welcome to the Caesar cipher <br>";
+echo "Message = " . $message . ", Key = " . $key . "\n";
 
-echo "Message = " . $message . "<br>";
+echo "Encrypted Message = " . encrypt($message, $key) . "\n";
 
-echo "Key = " . $key . "<br>";
-
-echo "Encrypted Message = " . encrypt($message, $key) . "<br>";
-
-echo "Decrypted Message is = " . decrypt(encrypt($message, $key), $key);
+echo "Decrypted Message = " . decrypt(encrypt($message, $key), $key);
